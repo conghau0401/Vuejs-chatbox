d597e0eb8059e88921efca370c7e002ee5115f0d
conghau0401 <conghau0401@gmail.com>
2022-05-21 10:42:24 +0700

    add data

--- src/App.vue
+++ src/App.vue
@@ -13,13 +13,62 @@ export default {
   data() {
     return {
       user: {
-        id: 10,
-        name: "Cong Hau",
-        avatar_url: "https://fdsfds.com/fdf.png"
+        id: Number,
+        name: String,
+        avatar: "/img/avatar1.jpeg",
+        itsMe: Number,
       },
       slug: "1"
     }
-  }
+  },
+  async mounted() {
+    //set idUser cookie
+    function setCookie(cname, cvalue, exdays) {
+      const d = new Date();
+      d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+      let expires = "expires="+d.toUTCString();
+      document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    }
+
+    //get idUser cookie
+    function getCookie(cname) {
+      let name = cname + "=";
+      let decodedCookie = decodeURIComponent(document.cookie);
+      let ca = decodedCookie.split(';');
+      for(let i = 0; i <ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+          c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+          return c.substring(name.length, c.length);
+        }
+      }
+      return "";
+    }
+
+    //check isset idUser
+    if (document.cookie.indexOf('idUser') == -1 ) {
+      setCookie("idUser", Math.floor(Math.random() * 90000) + 10000, 365);
+      setCookie("nameUser", createNameUser(5), 365);
+    }
+
+    //assign id
+    this.user.id = getCookie("idUser");
+    this.user.itsMe = getCookie("idUser");
+    this.user.name = getCookie("nameUser");
+
+    //Create User name
+    function createNameUser(length) {
+        var result           = '';
+        var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+        var charactersLength = characters.length;
+        for ( var i = 0; i < length; i++ ) {
+          result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      }
+      return result;
+    }
+  },
 };
 </script>
 
--- src/components/ChatBox.vue
+++ src/components/ChatBox.vue
@@ -9,7 +9,7 @@
         <li>Matching</li>
       </ul>
     </div>
-    <chat-list :msgs="msgsData" :itsMe="itsMe"></chat-list>
+    <chat-list :msgs="msgsData" :itsMe="this.user.itsMe"></chat-list>
     <chat-form @submitMessage="sendMessage"></chat-form>
   </div>
 </template>
@@ -29,25 +29,21 @@ export default {
     this.firebaseService = new TutorialDataService(this.roomId);
 
     return {
-      id: Number,
-      name: String,
-      myAvatar: "/img/avatar1.jpeg",
-      itsMe: Number,
       date: Date(),
       msgsData: [],
-      startDay: new Date('2022-01-01'),
     };
   },
   props: {
     roomId: Number,
+    user: Object,
   },
   methods: {
     sendMessage(msg) {
       var data = {
         from: {
-          id: this.id,
-          name: this.name,
-          avatar: this.myAvatar
+          id: this.user.id,
+          name: this.user.name,
+          avatar: this.user.avatar
         },
         date: this.date,
         msg: msg
@@ -85,60 +81,9 @@ export default {
       this.msgsData = _msgs;
     },
   },
-  watch: {
-    user: function(user) {
-      this.msgsData = { ...user };
-    },
-  },
-  async mounted() {
+  mounted() {
     //call response to API
     this.firebaseService.getAll().on("value", this.onDataChange);
-
-    //set idUser cookie
-    function setCookie(cname, cvalue, exdays) {
-      const d = new Date();
-      d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-      let expires = "expires="+d.toUTCString();
-      document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-    }
-
-    //get idUser cookie
-    function getCookie(cname) {
-      let name = cname + "=";
-      let decodedCookie = decodeURIComponent(document.cookie);
-      let ca = decodedCookie.split(';');
-      for(let i = 0; i <ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) == ' ') {
-          c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-          return c.substring(name.length, c.length);
-        }
-      }
-      return "";
-    }
-
-    //check isset idUser
-    if (document.cookie.indexOf('idUser') == -1 ) {
-      setCookie("idUser", Math.floor(Math.random() * 90000) + 10000, 365);
-      setCookie("nameUser", createNameUser(5), 365);
-    }
-
-    //assign id
-    this.id = this.itsMe = getCookie("idUser");
-    this.name = getCookie("nameUser");
-
-    //Create User name
-    function createNameUser(length) {
-        var result           = '';
-        var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-        var charactersLength = characters.length;
-        for ( var i = 0; i < length; i++ ) {
-          result += characters.charAt(Math.floor(Math.random() * charactersLength));
-      }
-      return result;
-    }
   },
 };
 </script>
